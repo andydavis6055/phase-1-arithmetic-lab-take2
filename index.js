01e383a2db4f2bd5aec5c8ed21ecb71b3abc1924
Andy Davis <andydavis6055@gmail.com>
2023-03-23 08:22:46 -0600

    Tests passing

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31
+let num2 = 2 
+const multiply = num1 * num2
+let random = Math.floor(Math.random() * 20) + 1 
+var num3 = 13;
+var num4 = 3;
+var mod = ((num3 * num4) + 4) % num3;
+let max = Math.max(1, 2, 3, 20)
